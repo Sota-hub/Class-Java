56bd966194ab895ae9676497eb9236a13c3287a5
SOTA <sota00@outlook.jp>
2021-10-28 20:44:34 -0700

    fix comments

--- currencyConverter/src/Converter.java
+++ currencyConverter/src/Converter.java
@@ -46,26 +46,36 @@ public class Converter {
 	
 	public static void sendRequest(String from, String to, double amount) throws IOException {
 		
-//		
+//		Send the request & Get the response (https://free.currencyconverterapi.com/)
 		URL url = new URL("https://free.currconv.com/api/v7/convert?q=" + from + "_" + to + "&compact=ultra&apiKey=b453fee223d5a7dbf708");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setRequestMethod("GET");
 		connection.connect();
 		int responseOriginal = connection.getResponseCode(); 
 		
+//		Read the response data & Encode it
 		if (responseOriginal == HttpURLConnection.HTTP_OK) {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuffer response = new StringBuffer();
 			
+//			BufferReader = read the text file
+//			InputStreamReader = make input data to string data
+//			getInputStream() = return input data
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
+//			StringBuffer() = Concatenate the string data
+//			readLine() = read the text file line by line
+//			response.append(input) = StringBuffer(input);
+			StringBuffer response = new StringBuffer();
 			String input;
 			while((input = reader.readLine()) != null) {
 				response.append(input);
 			} reader.close();
 			
+//			Parse the data (https://github.com/stleary/JSON-java)
+//			object = {"USD_CAD":1.23547} 
 			JSONObject object = new JSONObject(response.toString());
 			Double rate = object.getDouble(from + "_" + to);
-			
-			System.out.println(amount + from + " = " + amount * rate + to);
+
+			System.out.println("\n" +amount + from + " = " + amount * rate + to);
 			
 		} else {
 			System.out.println("Something went wrong!");
